d9175270537f30bb2f5b0cbd721a6a45443e792d
Hrodvitnir-Fenrir <44929201+Hrodvitnir-Fenrir@users.noreply.github.com>
2024-08-17 18:19:00 +0200

    feat: Add squad management functionality for creating and leaving squads

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { ActivityType, Client, Events, Guild, Message, TextChannel } from "discord.js";
+import { ActivityType, Client, EmbedBuilder, Events, Guild, Message, TextChannel } from "discord.js";
 import { config } from "dotenv";
 import * as path from "path";
 import { initMessage } from "./pannelManager/initMessage";
 import { getTheLastEvent } from "./pannelManager/scheduleFetcher";
 import { verificationUpdate } from "./pannelManager/updatePannel";
+import { createSquad, removeUser, updateMessage, userRoleManager } from "./squadTweeker/squadManager";
 config({ path: path.resolve(__dirname, "..", ".env") });
 
 export const client = new Client({ intents: ["Guilds"] });
@@ -17,8 +18,45 @@ client.once("ready", async () => {
 	const channel = await guild.channels.fetch("1273505346979102832") as TextChannel;
 	messagePannel = await initMessage(channel);
 
+	// setInterval(async () => {
+	// 	const event = await getTheLastEvent(guild);
+	// 	await verificationUpdate(event, messagePannel);
+	// }, 10 * 60 * 1000);
+
 	const event = await getTheLastEvent(guild);
 	await verificationUpdate(event, messagePannel);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isButton()) return;
+
+	if (interaction.customId === "createSquad") {
+		createSquad(interaction);
+	}
+
+	if (interaction.customId === "leaveSquad") {
+		const fields = await removeUser(interaction);
+		await updateMessage(interaction, fields);
+	}
+
+	if (interaction.customId === "Alpha" || interaction.customId === "Bretelle" || interaction.customId === "Camelote" || interaction.customId === "Delirium" || interaction.customId === "E") {
+		const fields = await removeUser(interaction);
+		fields.forEach(filed => {
+			if (filed.name === interaction.customId) {
+				filed.value += `\n<@${interaction.user.id}>`;
+			}
+		});
+		await updateMessage(interaction, fields);
+	}
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isStringSelectMenu()) return;
+
+	if (interaction.customId === "roleSelection") {
+		userRoleManager(interaction);
+		}
+
 });
 
 client.login(process.env.DISCORD_TOKEN);
