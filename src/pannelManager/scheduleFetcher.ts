18fbeb7b790af468981d9c7322fb94d4cc794a07
Hrodvitnir-Fenrir <44929201+Hrodvitnir-Fenrir@users.noreply.github.com>
2024-08-18 11:43:47 +0200

    Refactor scheduleFetcher module to use axios for fetching the last event in a guild

--- src/pannelManager/scheduleFetcher.ts
+++ src/pannelManager/scheduleFetcher.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { Collection, ComponentAssertions, Guild, GuildScheduledEvent, GuildScheduledEventStatus } from "discord.js";
 
-export async function getTheLastEvent(guild: Guild): Promise<GuildScheduledEvent<GuildScheduledEventStatus> | null> {
+export interface eventStructure {
+	id: string;
+	scheduledStartTime: number;
+	canceled: boolean;
+	name: string;
+}
+
+export async function getTheLastEvent(guild: Guild): Promise<eventStructure | null> {
 	try {
 		const events = await axios.get(
 			`https://discord.com/api/guilds/${guild.id}/scheduled-events`,
@@ -16,43 +23,21 @@ export async function getTheLastEvent(guild: Guild): Promise<GuildScheduledEvent
 			return {
 				id: event.id,
 				scheduledStartTime: event.guild_scheduled_event_exceptions[0]?.scheduled_start_time ? Date.parse(event.guild_scheduled_event_exceptions[0].scheduled_start_time) : Date.parse(event.scheduled_start_time),
-				canceled: event.guild_scheduled_event_exceptions[0]?.is_canceled || false
+				canceled: event.guild_scheduled_event_exceptions[0]?.is_canceled || false,
+				name: event.name
 			};
 		});
 
-		let id;
+		eventsArray.sort((a, b) => a.scheduledStartTime - b.scheduledStartTime);
 
-		if (eventsArray[0].canceled && eventsArray[1].canceled) {
-			return null;
-		} else if (eventsArray[0].canceled) {
-			id = eventsArray[1].id;
-		} else if (eventsArray[1].canceled) {
-			id = eventsArray[0].id;
-		} else {
-			id = eventsArray[0].scheduledStartTime < eventsArray[1].scheduledStartTime ? eventsArray[0].id : eventsArray[1].id;
+		for (const event of eventsArray) {
+			if (!event.canceled) {
+				return event;
+			}
 		}
-
-		let event = await guild.scheduledEvents.fetch(id) as GuildScheduledEvent<GuildScheduledEventStatus>;
-		return event;
-
-		// Need to send the time form here and not the id, the fetch is really broken
-
+		return null;
 	} catch (error) {
 		console.error(error);
 		return null;
 	}
-
-
-
-
-
-	// const events = await guild.scheduledEvents.fetch();
-
-	// const eventsArray = Array.from(events.values());
-	// eventsArray.sort((a, b) => a.scheduledStartTimestamp - b.scheduledStartTimestamp);
-	// const mostRecentEvent = eventsArray[0];
-
-	// console.log(eventsArray);
-
-	// return mostRecentEvent;
 }
